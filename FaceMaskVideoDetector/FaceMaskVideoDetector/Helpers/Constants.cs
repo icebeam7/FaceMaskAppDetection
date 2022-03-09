cac500d877ac6b2296ce9d0875690ce2de2930d7
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2022-03-09 01:17:02 +0100

    Add Project Source Code

---
+++ FaceMaskVideoDetector/FaceMaskVideoDetector/Helpers/Constants.cs
@@ -0,0 +1,24 @@
+﻿namespace FaceMaskVideoDetector.Helpers
+{
+    public class Constants
+    {
+        public static readonly string SubscriptionKey = "";
+        public static readonly string AccountId = "";
+        public static readonly string Location = "trial";
+
+        public static readonly string AuthBaseUrl = $"https://api.videoindexer.ai/Auth/{Location}/Accounts/";
+        public static readonly string TokenService = "AccessToken?allowEdit=True";
+        public static string VideoIndexerAccessToken = "";
+
+        public static readonly string VideoIndexerBaseUrl = $"https://api.videoindexer.ai/{Location}/Accounts";
+
+        public static readonly string UploadVideo = "Videos?privacy=Public&accessToken";
+        public static readonly string NameParameter = "name";
+
+        public static readonly string ListVideos = "Videos?accessToken";
+
+        public static readonly string CustomVisionBaseUrl = "";
+        public static readonly string CustomVisionKey = "";
+        public static readonly string CustomVisionService = "";
+    }
+}
---
+++ FaceMaskVideoDetector/FaceMaskVideoDetector/Views/UploadVideoView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FaceMaskVideoDetector.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UploadVideoView : ContentPage
+    {
+        public UploadVideoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
